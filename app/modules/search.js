5cac5a151383763be288a2c76d4b5cf891b8cd74
iansari <imran.iansari@gmail.com>
2012-07-03 15:01:14 -0400

    updates

--- app/modules/search.js
+++ app/modules/search.js
@@ -15,32 +15,7 @@ define([
 
         });*/
 
-        Search.Views.SearchCriteria = Backbone.View.extend({
-            template:"app/templates/search",
 
-            events:{
-                "click #search":"doSearch"
-            },
-
-            render:function (done) {
-                var view = this;
-
-                // Fetch the template, render it to the View element and call done.
-                app.fetchTemplate(this.template, function (tmpl) {
-                    view.el.innerHTML = tmpl({name:'imran'});
-
-                    // If a done function is passed, call it with the element
-                    if (_.isFunction(done)) {
-                        done(view.el);
-                    }
-                });
-            },
-
-            doSearch:function () {
-                app.app.trigger('search', {"test":"imran"});
-            }
-
-        });
 
         return Search;
 
